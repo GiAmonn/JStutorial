389471096c561d33da4a7a49b8a314d87a1cd53f
Giorgi Amonashvili <amonashviligiorgi1@gmail.com>
2023-09-30 17:03:37 +0400

    Create Week1A.js

---
+++ Week1A.js
@@ -0,0 +1,65 @@
+1) დაწერეთ პროგრამა რომელიც კონსოლში გამოიტანს "Hello World" -ს.
+
+console.log("Hello World");
+
+2) შექმენით ცვლადი და მიანიჭეთ მას რიცხვი. დაბეჭდეთ ცვლადის მნიშვნელობა.
+
+let num = 23;
+console.log(num);
+
+3) დაწერეთ პროგრამა ორი რიცხვის შესაკრებად და შედეგის გამოსატანად.
+
+let numOne = 5;
+let numTwo = 10;
+let result = numOne + numTwo;
+console.log(result);
+
+4) დაწერეთ პროგრამა რომელიც გამოაკლებს ორ რიცხვს ერთმანეთს და აჩვენებს შედეგს.
+
+let numX = 5;
+let numY = 10;
+let resultTwo = numY - numX;
+console.log(resultTwo);
+
+5)დაწერეთ პროგრამა ორი რიცხვის გასამრავლებლად და შედეგის გამოსატანად.
+
+let mulNumX = 5;
+let mulNumY = 10;
+let mulResult = mulNumX * mulNumY;
+console.log(mulResult);
+
+6) დაწერეთ პროგრამა ორი რიცხვის გასაყოფად და შედეგის გამოსატანად.
+
+let divNumX = 5;
+let divNumY = 10;
+let divResult = divNumY / divNumX;
+console.log(divResult);
+
+7) დაწერეთ პროგრამა ორი რიცხვის გაყოფის ნაშთის გამოსათვლელად.
+
+let pNumX = 3;
+let pNumY = 47;
+let pResult = pNumY % pNumX;
+console.log(pResult);
+
+8) შექმენით ცვლადი და მიანიჭეთ მას სტრიქონი. ამობეჭდეთ სტრიქონის სიგრძე.
+
+let text = "My name is Giorgi";
+console.log(text.length);
+
+9) დაწერეთ პროგრამა ორი სტრიქონის შესაერთებლად და შედეგის ჩვენების მიზნით.
+
+let textx = "sans moi tu es";
+let texty = " incomplet";
+let fullText = textx + texty;
+console.log(fullText);
+
+10) დაწერეთ პროგრამა სტრიქონის დიდ ასოებზე გადასაყვანად.
+
+let smallText = "I AM SMALL"
+console.log(smallText.toLowerCase())
+
+11) დაწერეთ პროგრამა სტრიქონის მცირე ასოზე გადასაყვანად.
+
+let upperText = "i am big"
+console.log(upperText.toUpperCase())
